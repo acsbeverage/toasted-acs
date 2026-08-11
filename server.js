83ed2219eb0401723f50a3a056098d12a7c587f6
acsbeverage <kevin@acsbeverage.com>
2026-08-11 10:17:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const fs      = require('fs');
 const app = express();
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: '50mb' }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/auth',   require('./routes/auth'));
 app.use('/api/orders', require('./routes/orders'));
